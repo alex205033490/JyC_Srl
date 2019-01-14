4bb2e603c0fbd1d2531aa3d70b878e9f5bb2c8fa
alex205033490 <ing.alex.oyola@gmail.com>
2019-01-14 01:35:55 -0400

    version 0.1
    primera ves

---
+++ JyC/src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+   loginpage1 = LoginPage
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
